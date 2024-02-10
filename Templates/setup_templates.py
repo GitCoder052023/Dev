c86e3a081037bca8af798efd3ea577e24b1c00e6
GitCoder052023 <K.alam93899@gmail.com>
2024-02-10 16:23:11 +0530

    adding templates

--- Templates/setup_templates.py
+++ Templates/setup_templates.py
@@ -1,15 +1,15 @@
 import os
 
 loc = os.getcwd()
-n = loc.replace("\\", "/")
+new_loc = loc.replace("\\", "/")
 
 
 def get_configs(filename):
-    with open(f"{n}/Templates/configs/HTML/{filename}", "r", encoding="utf-8") as file:
+    with open(f"{new_loc}/Dev/Templates/configs/HTML/{filename}", "r", encoding="utf-8") as file:
         html_content = file.read()
     return html_content
 
 
 def fetch_configs():
-    return os.listdir(f"{n}/Templates/configs/HTML/")
+    return os.listdir(f"{new_loc}/Templates/configs/HTML/")
 
