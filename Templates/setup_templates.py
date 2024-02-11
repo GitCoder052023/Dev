d30c900e385fea71c70dc0e1a2caad9a8ed77674
GitCoder052023 <K.alam93899@gmail.com>
2024-02-11 18:28:02 +0530

    adding setup configs function

--- Templates/setup_templates.py
+++ Templates/setup_templates.py
@@ -13,3 +13,38 @@ def get_configs(filename):
 def fetch_configs():
     return os.listdir(f"{new_loc}/Dev/Templates/configs/HTML/")
 
+
+def setup_configs():
+    flask_app = """
+from flask import Flask, render_template
+import Dev.Templates.setup_templates as ds
+import os
+
+app = Flask(__name__)
+
+os.makedirs('templates', exist_ok=True)
+
+html = ds.get_configs("general.html")
+folder_path = "templates"
+file_name = "index.html"
+file_path = os.path.join(folder_path, file_name)
+
+with open(file_path, "w") as f:
+    f.write(html)
+        
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+            """
+
+    os.makedirs('static', exist_ok=True)  # Create 'static' directory if it doesn't exist
+    os.makedirs('templates', exist_ok=True)  # Create 'templates' directory if it doesn't exist
+
+    # Write basic Flask app code to a new Python file
+    with open('app.py', 'w') as f:
+        f.write(flask_app)
+
