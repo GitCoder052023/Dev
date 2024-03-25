117e788d26f04e354c66a06c78a0ee29fdfe9fca
GitCoder052023 <K.alam93899@gmail.com>
2024-03-25 11:37:07 +0530

    removing __init__.py

--- Templates/Web/setup_templates.py
+++ Templates/Web/setup_templates.py
@@ -1,42 +1,23 @@
 import os
-import site
 
 loc = os.getcwd()
 new_loc = loc.replace("\\", "/")
-venv_templates_path = f"{new_loc}/.venv/Lib/site-packages/Templates"
-# Get the global site-packages directory
-site_packages_path = site.getsitepackages()[0]
-global_templates_path = f"{site_packages_path}/Templates"
 
 
 def get_configs(filename):
-    if os.path.exists(venv_templates_path):
-        template_folder = venv_templates_path
-    elif os.path.exists(global_templates_path):
-        template_folder = global_templates_path
-    else:
-        return "Error: Templates folder not found"
-
-    with open(f"{template_folder}/Web/configs/HTML/{filename}", "r", encoding="utf-8") as file:
+    with open(f"{new_loc}/Dev/Templates/Web/configs/HTML/{filename}", "r", encoding="utf-8") as file:
         html_content = file.read()
     return html_content
 
 
 def fetch_configs():
-    if os.path.exists(venv_templates_path):
-        template_folder = venv_templates_path
-    elif os.path.exists(global_templates_path):
-        template_folder = global_templates_path
-    else:
-        return "Error: Templates folder not found"
-
-    return os.listdir(f"{template_folder}/configs/HTML/")
+    return os.listdir(f"{new_loc}/Dev/Templates/configs/HTML/")
 
 
 def setup_configs():
     flask_app = """
 from flask import Flask, render_template
-import Templates.setup_templates as ds
+import Dev.Templates.setup_templates as ds
 import os
 
 app = Flask(__name__)
